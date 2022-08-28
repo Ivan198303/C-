39f33efa2dd3024efbd0259d4816acddc6df887a
Chernyh Ivan <blackvano@rambler.ru>
2022-08-28 19:52:14 +0500

    test5

---
+++ test5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Привествие по определенному условию
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
