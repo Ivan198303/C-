c55bf108757e9d042b61ff8605c4ea52dd77dbbc
Chernyh Ivan <blackvano@rambler.ru>
2022-08-28 19:29:18 +0500

    test3+test4

--- test3/Program.cs
+++ test3/Program.cs
@@ -3,5 +3,5 @@ int number1 = new Random().Next(1, 50); // Случайное число от 1 
 Console.WriteLine(number1);
 int number2 = new Random().Next(1, 50); // Случайное число от 1 до 49
 Console.WriteLine(number2);
-int sum = number1 + number2;
-Console.WriteLine(sum);
+int summa = number1 + number2;
+Console.WriteLine(summa);
