83de0da9f7ccb0207844d610691e886f41ffcc0c
Chernyh <blackvano@rambler.ru>
2022-09-20 09:46:53 +0500

    lekciy4

---
+++ lekciy4/test1/Program.cs
@@ -0,0 +1,28 @@
+﻿// двумерный массив для печати и рандомного заполнения
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) //  Количество строк это относится к первой цифре new int[3, 4];
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // Количество столбцов это относится ко второй цифре new int[3, 4];
+    {
+        {
+            matr[i, j] = new Random().Next(1, 10);  //[1; 10) диапазон цифр для заполнения
+        }
+    }
+}
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();  
+PrintArray(matrix);
---
+++ lekciy4/test3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Вычисление факториала
+double Factorial(int n) // так как если при использовании int Factorial(int n) мы получаем переполнение используется тип данных double
+{
+    // 1! = 1 
+    // 0! = 1 
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+// вычисление числа фибоначи
+// f(1) = 1 
+// f(2) = 1 
+// f(n) = f(n-1) + f(n-2)
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+    for (int i = 1; i < 50; i++)
+    {
+        Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+    }
